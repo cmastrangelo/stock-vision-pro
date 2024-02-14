689f7a58f5d8d3d2e32758ac949d264669232f66
Carlos <carlos.mastrangelo@gmail.com>
2024-02-13 18:41:15 -0800

    debug false

--- soozhub/settings.py
+++ soozhub/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ie$(^%&(7z7!&b!h0yh%wk3rf=fu(c@v=h!5xc4==s+)k7-cm-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
