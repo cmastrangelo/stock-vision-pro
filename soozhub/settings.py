688a0debce31fa5b8beb4262cbb8f102e59732db
Carlos <carlos.mastrangelo@gmail.com>
2024-02-13 18:43:46 -0800

    whitenoise remove

--- soozhub/settings.py
+++ soozhub/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 #STATICFILES_DIRS = [
